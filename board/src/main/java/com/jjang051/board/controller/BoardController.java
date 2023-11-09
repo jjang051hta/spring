f92e352e97a7e2dec7bdc18617268cf7544fd1fb
jjang051hta <jjang051hta@gmail.com>
2023-11-09 11:41:15 +0900

    redirectAttribute를 이용한 모달 띄우기

--- board/src/main/java/com/jjang051/board/controller/BoardController.java
+++ board/src/main/java/com/jjang051/board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.jjang051.board.controller;
 
 import com.jjang051.board.dto.BoardDto;
+import com.jjang051.board.dto.ModalDto;
 import com.jjang051.board.service.BoardService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,8 +23,8 @@ import java.util.List;
 public class BoardController {
 
 
-    /*@Autowired
-    BoardService boardService;*/
+//    @Autowired
+//    BoardService boardService;
 
     private final BoardService boardService;
 
@@ -36,21 +38,34 @@ public class BoardController {
     @GetMapping("/write")
     public String write(Model model) {
         //model.addAttribute("title","write");
-        model.addAttribute("boardDto", new BoardDto());
+        BoardDto boardDto = BoardDto.builder().build();
+        model.addAttribute("boardDto", boardDto);
         return  "/board/write";
     }
 
     @PostMapping("/write")
     public String writeProcess(@Valid @ModelAttribute BoardDto boardDto,
                                BindingResult bindingResult,
-                               Model model
+                               Model model,
+                               RedirectAttributes redirectAttributes
                                ) {
         if(bindingResult.hasErrors()){
             model.addAttribute("boardDto", boardDto);
             log.info("boardDto=={}",boardDto.toString());
             return "/board/write";
         }
-        boardService.insertBoard(boardDto);
+        int result = boardService.insertBoard(boardDto);
+        if(result>0){
+            ModalDto modalDto = ModalDto.builder()
+                    .isState("success")
+                    .title("글쓰기")
+                    .msg("글이 입력되었습니다.")
+                    .build();
+            redirectAttributes.addFlashAttribute("modalDto",modalDto);
+        }
+        //redirectAttributes.addFlashAttribute("name",boardDto.getName());
+        //redirectAttributes.addAttribute("age",20);
+
         return "redirect:/";
     }
 
--- board/src/main/java/com/jjang051/board/dto/BoardDto.java
+++ board/src/main/java/com/jjang051/board/dto/BoardDto.java
@@ -2,6 +2,7 @@ package com.jjang051.board.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -12,17 +13,17 @@ import lombok.*;
 @Builder
 @ToString
 public class BoardDto {
-
+    //NotNull null허용 금지   NotEmpty  NotNull+""  NotBlank NotEmpty "   "
     private int id;
 
-    @NotEmpty(message = "이름은 필수 입력사항입니다. 제발 입력해주세요.")
+    @NotEmpty(message = "이름은 필수 입력사항입니다.")
     private String name;
 
     @NotBlank(message = "제목은 필수 입력사항입니다.")
     @Size(min=5,max=100, message = "최소5글자 이상 최대 100자까지 가능합니다.")
     private String title;
 
-    @NotBlank(message = "내용은 필수입력사항입니다.")
+    @NotBlank(message = "내용은 필수 입력사항입니다.")
     @Size(min=10,max=1000, message = "최소10글자 이상 최대 1000자까지 가능합니다.")
     private String content;
 
