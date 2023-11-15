13fe8ff06b7f76a8aa6e36ed77bb59154c8f1cde
jjang051hta <jjang051hta@gmail.com>
2023-11-15 14:06:15 +0900

    visual swiper 처리

--- gallery/src/main/java/com/jjang051/gallery/config/WebConfig.java
+++ gallery/src/main/java/com/jjang051/gallery/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.jjang051.gallery.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
