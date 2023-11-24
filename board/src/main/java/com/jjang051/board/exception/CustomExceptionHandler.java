4c7576dce67823d4f05235b046b38c39faa5f373
jjang051hta <jjang051hta@gmail.com>
2023-11-24 11:21:17 +0900

    오류

--- board/src/main/java/com/jjang051/board/exception/CustomExceptionHandler.java
+++ board/src/main/java/com/jjang051/board/exception/CustomExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.jjang051.board.exception;
 
+import com.jjang051.board.code.ErrorCode;
 import com.jjang051.board.dto.ErrorDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.SQLException;
 
 
 @ControllerAdvice
@@ -20,12 +24,22 @@ public class CustomExceptionHandler {
         return "/errors/error";
     }
 
-    @ExceptionHandler(MemberException.class)
+    @ExceptionHandler(SQLException.class)
     @ResponseBody
-    public ErrorDto duplicateMember(MemberException e) {
+    public ErrorDto duplicateMember(SQLException e) {
         ErrorDto response = ErrorDto.builder()
-                .errorCode(e.getErrorCode())
-                .errorMessage(e.getDetailMessage())
+                .errorCode(ErrorCode.DUPLICATE_MEMBER)
+                .errorMessage("알 수 없는 에러로 회원가입이 되지 않았습니다.")
+                .build();
+        return response;
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseBody
+    public ErrorDto anonymousException(RuntimeException e) {
+        ErrorDto response = ErrorDto.builder()
+                .errorCode(ErrorCode.INTERNAL_SERVER_ERROR)
+                .errorMessage(ErrorCode.INTERNAL_SERVER_ERROR.getMessage())
                 .build();
         return response;
     }
