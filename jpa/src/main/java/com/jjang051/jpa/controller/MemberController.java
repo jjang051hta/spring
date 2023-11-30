7e4ee7aaede02fbc6d8ff62590af4c5b5447b92e
jjang051hta <jjang051hta@gmail.com>
2023-11-30 15:32:32 +0900

    jpa

--- jpa/src/main/java/com/jjang051/jpa/controller/MemberController.java
+++ jpa/src/main/java/com/jjang051/jpa/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.jjang051.jpa.controller;
 
+import com.jjang051.jpa.dto.MemberDto;
 import com.jjang051.jpa.entity.Member02;
 import com.jjang051.jpa.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -22,14 +23,13 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String joinProcess(Member02 member) {
-        memberService.join(member);
+    public String joinProcess(MemberDto memberDto) {
+        memberService.join(memberDto);
         return "redirect:/";
     }
-
     @GetMapping("/list")
     public String list(Model model) {
-        List<Member02> memberList = memberService.getAllMember();
+        List<MemberDto> memberList = memberService.getAllMember();
         model.addAttribute("memberList",memberList);
         return "/member/list";
     }
--- jpa/src/main/java/com/jjang051/jpa/service/MemberService.java
+++ jpa/src/main/java/com/jjang051/jpa/service/MemberService.java
@@ -1,10 +1,12 @@
 package com.jjang051.jpa.service;
 
+import com.jjang051.jpa.dto.MemberDto;
 import com.jjang051.jpa.entity.Member02;
 import com.jjang051.jpa.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,13 +14,26 @@ import java.util.List;
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    public Member02 join(Member02 member) {
-        Member02 responseMember = memberRepository.save(member);
-        return responseMember;
+    public MemberDto join(MemberDto memberDto) {
+        Member02 dbJoinMember = Member02.builder()
+                .userId(memberDto.getUserId())
+                .email(memberDto.getEmail())
+                .nickName(memberDto.getNickName())
+                .age(memberDto.getAge())
+                .build();
+        Member02 responseMember = memberRepository.save(dbJoinMember);
+        MemberDto responseMemberDto = MemberDto.fromEntity(responseMember);
+        return responseMemberDto;
     }
 
-    public List<Member02> getAllMember() {
-        List<Member02> memberList = memberRepository.findAll();
+    public List<MemberDto> getAllMember() {
+        // stream과 lamda를 쓰면 이걸 간단히 줄일수 있다....
+        List<Member02> member02List = memberRepository.findAll();
+        List<MemberDto> memberList = new ArrayList<>();
+        for(int i=0;i<member02List.size();i++) {
+            memberList.add(MemberDto.fromEntity(member02List.get(i)));
+        }
+        // 반목문 돌려서 dto memberList에 담기...
         return  memberList;
     }
 }
