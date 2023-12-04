d3013fab1591806999ad335bf4bf0741b0f7203f
jjang051hta <jjang051hta@gmail.com>
2023-12-04 11:45:21 +0900

    jpa

--- jpa/src/main/java/com/jjang051/jpa/controller/CommentController.java
+++ jpa/src/main/java/com/jjang051/jpa/controller/CommentController.java
@@ -2,11 +2,13 @@ package com.jjang051.jpa.controller;
 
 
 import com.jjang051.jpa.dto.BoardDto;
+import com.jjang051.jpa.dto.CommentDto;
 import com.jjang051.jpa.entity.Board02;
 import com.jjang051.jpa.entity.Comment02;
 import com.jjang051.jpa.service.BoardService;
 import com.jjang051.jpa.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,10 +16,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/comment")
 @RequiredArgsConstructor
+@Slf4j
 public class CommentController {
     private final CommentService commentService;
     private final BoardService boardService;
@@ -40,7 +44,8 @@ public class CommentController {
 
     @PostMapping("/ajaxinsert/{id}")
     @ResponseBody
-    public Map<String,String> ajaxinsert(@PathVariable("id") int id, @RequestParam String content) {
+    public Map<String, Object> ajaxinsert(@PathVariable("id") int id, @RequestParam String content) {
+        log.info("id===={},content===={}",id,content);
         BoardDto boardDto = boardService.getBoard(id);
         Board02 board02 = Board02.builder()
                 .id(boardDto.getId())
@@ -50,12 +55,19 @@ public class CommentController {
                 .commentList(boardDto.getCommentList())
                 .subject(boardDto.getSubject())
                 .build();
-        commentService.insertComment(board02,content);
-        Map<String,String> resultMap = new HashMap<>();
+        // save된 entity를 리턴받을 수 있다.
+        Comment02 comment02 = commentService.insertAjaxComment(board02,content);
+        // 데이터를 전달하기 위해 dto로 변환해서
+        CommentDto responseComment = CommentDto.fromEntity(comment02);
+
+        Map<String,Object> resultMap = new HashMap<>();
         resultMap.put("isInsert","ok");
-        resultMap.put("content","db에서 들고온 값");
-        resultMap.put("id","10");
-        resultMap.put("date","2023-12-03 10:51");
+        // map에 실어서 보낸다.
+        resultMap.put("responseComment",responseComment);
+
+//        resultMap.put("content",responseComment.getContent());
+//        resultMap.put("id",responseComment.getId().toString());
+//        resultMap.put("date",responseComment.getCreateDate().toString());
         return resultMap;
     }
     @DeleteMapping("/delete/{id}")
