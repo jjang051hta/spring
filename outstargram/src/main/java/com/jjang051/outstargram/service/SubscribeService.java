2d30fbc7f34c9b59c6283d700fb51b735fdb0479
jjang051hta <jjang051hta@gmail.com>
2023-12-12 16:07:10 +0900

    blog

--- outstargram/src/main/java/com/jjang051/outstargram/service/SubscribeService.java
+++ outstargram/src/main/java/com/jjang051/outstargram/service/SubscribeService.java
@@ -2,18 +2,17 @@ package com.jjang051.outstargram.service;
 
 import com.jjang051.outstargram.repository.SubscribeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SubscribeService {
 
     private final SubscribeRepository subscribeRepository;
-    @Transactional
-    public void subscribe(int fromMemberId, int toMemberId) {
-        subscribeRepository.subscribe(fromMemberId,toMemberId);
-    }
+
 
     @Transactional
     public int subscribeCount(int memberId) {
@@ -22,7 +21,13 @@ public class SubscribeService {
 
 
     @Transactional
-    public void unSubscribe(int customerDetailsId, int urlId) {
+    public void subscribe(int customerDetailsId, int urlId) {
+        log.info("subscribe===");
         subscribeRepository.subscribe(customerDetailsId,urlId);
+    }
+    @Transactional
+    public void unSubscribe(int customerDetailsId, int urlId) {
+        log.info("unSubscribe===");
+        subscribeRepository.unSubscribe(customerDetailsId,urlId);
     }
 }
